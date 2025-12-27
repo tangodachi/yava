e21896cf6d5d439490c0bba223845a61830ade65
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-28 00:31:21 +0100

    expect source requestSignInCode on requestSignInCode

--- composeApp/src/commonMain/kotlin/com/tangodachi/yava/authentication/AuthenticationImplementation.kt
+++ composeApp/src/commonMain/kotlin/com/tangodachi/yava/authentication/AuthenticationImplementation.kt
@@ -3,8 +3,8 @@ package com.tangodachi.yava.authentication
 import org.koin.core.annotation.Single
 
 @Single(binds = [Authentication::class])
-class AuthenticationImplementation : Authentication {
+class AuthenticationImplementation(private val source: AuthenticationSource) : Authentication {
     override fun requestSignInCode(email: String) {
-        TODO()
+        source.requestSignInCode("q@q.be")
     }
 }
