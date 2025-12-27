380223f09a235bd044ed58dd5107b0e7d0e0da11
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-28 00:49:36 +0100

    configure serialization

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 kotlin {
@@ -29,7 +31,7 @@ kotlin {
     
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            implementation(libs.kotlinx.serialization.json)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
