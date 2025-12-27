5f8d2cbc15c753116e319eceded99de2f3499b60
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-27 22:07:34 +0100

    Navigate to Sign In

--- composeApp/src/commonMain/kotlin/com/tangodachi/yava/App.kt
+++ composeApp/src/commonMain/kotlin/com/tangodachi/yava/App.kt
@@ -1,10 +1,29 @@
 package com.tangodachi.yava
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.tangodachi.design.Scaffold
+import com.tangodachi.design.Text
+import com.tangodachi.design.TopBar
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import yava.composeapp.generated.resources.Res
+import yava.composeapp.generated.resources.top_bar_title_sign_in
 
 @Composable
 @Preview
 fun App() {
-
+    Scaffold(
+        topBar = {
+            TopBar(
+                title = { Text(stringResource(Res.string.top_bar_title_sign_in)) }
+            )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            SignInPage()
+        }
+    }
 }
