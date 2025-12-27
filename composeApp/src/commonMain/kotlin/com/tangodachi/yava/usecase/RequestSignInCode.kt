15a60dca707b19acddf171458a9e78d1d62e61fc
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-27 23:32:42 +0100

    create RequestSignInCode

---
+++ composeApp/src/commonMain/kotlin/com/tangodachi/yava/usecase/RequestSignInCode.kt
@@ -0,0 +1,11 @@
+package com.tangodachi.yava.usecase
+
+import org.koin.core.annotation.Factory
+
+@Factory
+class RequestSignInCode {
+    operator fun invoke(email: String) {
+        TODO()
+    }
+}
+
