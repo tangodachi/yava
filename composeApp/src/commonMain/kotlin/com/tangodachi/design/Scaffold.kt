2744a05657cc7c36098d99bff6e6c4a5e07664a8
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-27 21:56:57 +0100

    Create Scaffold

---
+++ composeApp/src/commonMain/kotlin/com/tangodachi/design/Scaffold.kt
@@ -0,0 +1,19 @@
+package com.tangodachi.design
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Scaffold(
+    modifier: Modifier = Modifier,
+    topBar: @Composable () -> Unit = {},
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        content = content
+    )
+}
