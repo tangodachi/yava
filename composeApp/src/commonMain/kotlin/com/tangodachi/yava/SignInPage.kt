ba08ac47878c4178fa67e330080b55d58a31ab2a
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-27 22:20:27 +0100

    Add email TextField on Sign In page

--- composeApp/src/commonMain/kotlin/com/tangodachi/yava/SignInPage.kt
+++ composeApp/src/commonMain/kotlin/com/tangodachi/yava/SignInPage.kt
@@ -1,9 +1,24 @@
 package com.tangodachi.yava
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.tangodachi.design.Text
+import com.tangodachi.design.TextField
+import org.jetbrains.compose.resources.stringResource
+import yava.composeapp.generated.resources.Res
+import yava.composeapp.generated.resources.sign_in_label_email
 
 @Composable
 fun SignInPage() {
-    Text("Sign In Page")
+    TextField(
+        label = { Text(stringResource(Res.string.sign_in_label_email)) },
+        value = "",
+        onValueChange = {},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    )
 }
