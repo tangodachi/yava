d517e35d7712feeea2877a5ea1481042ffa78a57
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-28 01:01:14 +0100

    configure default request

--- composeApp/src/commonMain/kotlin/com/tangodachi/yava/Koin.kt
+++ composeApp/src/commonMain/kotlin/com/tangodachi/yava/Koin.kt
@@ -3,6 +3,7 @@ package com.tangodachi.yava
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.serialization.kotlinx.json.json
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
@@ -14,6 +15,11 @@ class AppModule {
     @Single
     fun provideHttpClient(): HttpClient {
         return provideHttpClient {
+            defaultRequest {
+                host = "localhost"
+                port = SERVER_PORT
+            }
+
             install(ContentNegotiation) {
                 json()
             }
