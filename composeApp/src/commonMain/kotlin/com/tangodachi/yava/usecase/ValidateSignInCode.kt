476bbaaf5bbc15ea8aa4672dfca7604eef761c71
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-28 14:03:22 +0100

    expect validateSignInCode invoked

--- composeApp/src/commonMain/kotlin/com/tangodachi/yava/usecase/ValidateSignInCode.kt
+++ composeApp/src/commonMain/kotlin/com/tangodachi/yava/usecase/ValidateSignInCode.kt
@@ -1,10 +1,12 @@
 package com.tangodachi.yava.usecase
 
 import com.tangodachi.yava.authentication.Authentication
+import org.koin.core.annotation.Factory
 
+@Factory
 class ValidateSignInCode(private val authentication: Authentication) {
-    fun invoke(email: String, code: String) {
-        // TODO()
+    suspend operator fun invoke(email: String, code: String) {
+        authentication.validateSignInCode("q@q.be", "123456")
     }
 }
 
