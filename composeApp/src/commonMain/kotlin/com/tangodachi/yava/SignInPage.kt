08b21255f2fbf3f2f3854925988329f9e53da919
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-28 13:06:18 +0100

    add sign in code field

--- composeApp/src/commonMain/kotlin/com/tangodachi/yava/SignInPage.kt
+++ composeApp/src/commonMain/kotlin/com/tangodachi/yava/SignInPage.kt
@@ -3,6 +3,7 @@ package com.tangodachi.yava
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,6 +23,7 @@ import com.tangodachi.yava.usecase.RequestSignInCode
 import kotlinx.coroutines.launch
 import yava.composeapp.generated.resources.Res
 import yava.composeapp.generated.resources.sign_in_action_request_code
+import yava.composeapp.generated.resources.sign_in_label_code
 import yava.composeapp.generated.resources.sign_in_label_email
 
 @Composable
@@ -55,6 +57,15 @@ fun SignInPage(
             label = { Text(stringResource(Res.string.sign_in_label_email)) },
             value = email,
             onValueChange = onEmailChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(stringResource(Res.string.sign_in_label_code)) },
             modifier = Modifier.fillMaxWidth()
         )
 
