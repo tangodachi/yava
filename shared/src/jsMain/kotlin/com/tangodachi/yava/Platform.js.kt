97988b90eba9a8e95e2e6ca891442b200dd8cbac
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2025-12-28 01:13:42 +0100

    clean up sample code

--- shared/src/jsMain/kotlin/com/tangodachi/yava/Platform.js.kt
+++
@@ -1,7 +0,0 @@
-package com.tangodachi.yava
-
-class JsPlatform: Platform {
-    override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
--- shared/src/jvmMain/kotlin/com/tangodachi/yava/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.tangodachi.yava
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
