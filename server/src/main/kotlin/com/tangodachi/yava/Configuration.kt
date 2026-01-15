71d33b2d3304f4c0fcda60509b8e046b8899d42f
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2026-01-15 16:41:52 +0100

    implement SendEmailImplementation

--- server/src/main/kotlin/com/tangodachi/yava/Configuration.kt
+++ server/src/main/kotlin/com/tangodachi/yava/Configuration.kt
@@ -1,3 +1,23 @@
 package com.tangodachi.yava
 
-object Configuration {}
+data class Configuration(val notification: Notification) {
+    data class Notification(val email: Email) {
+        data class Email(
+            val address: String,
+            val password: String,
+            val smtp: Smtp,
+        ) {
+            data class Smtp(
+                val auth: String,
+                val host: String,
+                val port: Int,
+                val socketFactory: SocketFactory,
+            ) {
+                data class SocketFactory(
+                    val `class`: String,
+                    val port: Int,
+                )
+            }
+        }
+    }
+}
--- server/src/main/kotlin/com/tangodachi/yava/Koin.kt
+++ server/src/main/kotlin/com/tangodachi/yava/Koin.kt
@@ -1,8 +1,10 @@
 package com.tangodachi.yava
 
 import io.ktor.server.application.Application
+import io.ktor.server.config.ApplicationConfig
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Provided
 import org.koin.core.annotation.Single
 import org.koin.core.context.startKoin
 import org.koin.ksp.generated.module
@@ -17,5 +19,25 @@ fun Application.koin() {
 @ComponentScan
 class AppModule {
     @Single
-    fun provideConfiguration() = Configuration
+    fun provideConfiguration(@Provided env: ApplicationConfig): Configuration {
+        return Configuration(
+            notification = Configuration.Notification(
+                email = Configuration.Notification.Email(
+                    address = env.property("notification.email.address").getString(),
+                    password = env.property("notification.email.password").getString(),
+                    smtp = Configuration.Notification.Email.Smtp(
+                        auth = env.property("notification.email.smtp.auth").getString(),
+                        host = env.property("notification.email.smtp.host").getString(),
+                        port = env.property("notification.email.smtp.port").getString().toInt(),
+                        socketFactory = Configuration.Notification.Email.Smtp.SocketFactory(
+                            `class` = env.property("notification.email.smtp.socketFactory.class")
+                                .getString(),
+                            port = env.property("notification.email.smtp.socketFactory.port")
+                                .getString().toInt(),
+                        ),
+                    ),
+                )
+            ),
+        )
+    }
 }
