8ab0b7478cb67422f68d252b6f339814d7f3052b
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2026-01-20 16:01:32 +0100

    expect send sign in code from sender 2

--- server/src/main/kotlin/com/tangodachi/yava/interactor/RequestSignInCode.kt
+++ server/src/main/kotlin/com/tangodachi/yava/interactor/RequestSignInCode.kt
@@ -8,13 +8,14 @@ import org.koin.core.annotation.Factory
 
 @Factory
 class RequestSignInCode(
+    private val configuration: Configuration,
     private val generateCode: GenerateCode,
     private val sendEmail: SendEmail,
 ) {
     suspend operator fun invoke(parameters: RequestSignInCodeParameters) {
         sendEmail(
             recipient = parameters.email,
-            sender = "q@q.be",
+            sender = configuration.notification.email.address,
             title = "",
             message = generateCode()
         )
