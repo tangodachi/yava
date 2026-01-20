8c397f4a9c7a64a432ced74b4178291fa0f4a1c3
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2026-01-20 16:06:25 +0100

    call RequestSignInCodeParameters on request-sign-in-code

--- server/src/main/kotlin/com/tangodachi/yava/routing.kt
+++ server/src/main/kotlin/com/tangodachi/yava/routing.kt
@@ -3,17 +3,21 @@ package com.tangodachi.yava
 import com.tangodachi.yava.authentication.AuthenticationApi
 import com.tangodachi.yava.authentication.RequestSignInCodeParameters
 import com.tangodachi.yava.authentication.ValidateSignInCodeParameters
+import com.tangodachi.yava.interactor.RequestSignInCode
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import org.koin.ktor.ext.get
 
 fun Application.routing() {
+    val requestSignInCode = get<RequestSignInCode>()
+
     routing {
         post(AuthenticationApi.requestSignInCode) {
-            println("Received ${call.receive<RequestSignInCodeParameters>()}")
+            requestSignInCode(call.receive<RequestSignInCodeParameters>())
 
             call.respond(HttpStatusCode.OK)
         }
